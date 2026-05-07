28ffc7091e9cc7ca1bb97296b734376b02b39de9
Willi Mezgolits <wmezgoli@c2r1p5.42vienna.com>
2026-05-07 21:55:11 +0200

    new

---
+++ ft_putendl_fd.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+#include <stddef.h>
+#include <unistd.h>
+
+void ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		write(fd, &s[i], 1);
+		i++;
+    }
+	write(fd, "\n", 1);
+}
+
+int main(void)
+{
+	int fd = 1;
+	char *s = "hallo";
+
+	ft_putendl_fd(s, fd);
+}
